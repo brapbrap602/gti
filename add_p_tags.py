6449c880b6b4c0489ca5b873c200cf290ee4349d
Henock T <teshome0@gmail.com>
2025-01-28 22:22:56 -0700

    dark mode colors. more chapters. errors=replace for encoding errs

--- add_p_tags.py
+++ add_p_tags.py
@@ -1,25 +1,40 @@
 from pathlib import Path
+
 import chardet
 
 CHAPTERS = Path(r"chapters")
 
-for chapter in CHAPTERS.iterdir():
-    # Detect the file encoding
-    with open(chapter, 'rb') as f:
-        raw_data = f.read()
-        detected_encoding = chardet.detect(raw_data)['encoding']
 
-    # Read using the detected encoding
-    with open(chapter, encoding=detected_encoding) as f:
-        data = f.read()
+def get_encoding(chapter):
+    detected_encoding = "utf-8"
+    try:
+        with open(chapter, 'rb') as f:
+            raw_data = f.read()
+            detected_encoding = chardet.detect(raw_data)['encoding']
+    except Exception as e:
+        print(e)
+    return detected_encoding
 
-    data = data.split("\n")
-    new_data = [f"<h1>{data[1]}</h1>"]
-    for d in data[1:]:
-        if d.strip():
-            new_data.append(f"<p>{d}</p>")
 
-    # Write back using the original encoding
-    with open(chapter, 'w', encoding=detected_encoding) as f:
-        print(f"writing to {chapter.stem} with encoding {detected_encoding}")
-        f.write("".join(new_data))
+def main():
+    for chapter in CHAPTERS.iterdir():
+        # Detect the file encoding
+        detected_encoding = get_encoding(chapter)
+        # Read using the detected encoding
+        with open(chapter, encoding=detected_encoding, errors="replace") as f:
+            data = f.read()
+
+        data = data.split("\n")
+        new_data = [f"<h1>{data[1]}</h1>"]
+        for d in data[1:]:
+            if d.strip():
+                new_data.append(f"<p>{d}</p>")
+
+        # Write back using the original encoding
+        with open(chapter, 'w', encoding=detected_encoding, errors="replace") as f:
+            print(f"writing to {chapter.stem} with encoding {detected_encoding}")
+            f.write("".join(new_data))
+
+
+if __name__ == '__main__':
+    main()
