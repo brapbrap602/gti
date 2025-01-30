0970f56ce5c3c742a8cec6791a28f286a5d45d5c
Henock T <teshome0@gmail.com>
2025-01-29 21:02:01 -0700

    remove duped titles

--- add_p_tags.py
+++ add_p_tags.py
@@ -8,9 +8,9 @@ CHAPTERS = Path(r"chapters")
 def get_encoding(chapter):
     detected_encoding = "utf-8"
     try:
-        with open(chapter, 'rb') as f:
+        with open(chapter, "rb") as f:
             raw_data = f.read()
-            detected_encoding = chardet.detect(raw_data)['encoding']
+            detected_encoding = chardet.detect(raw_data)["encoding"]
     except Exception as e:
         print(e)
     return detected_encoding
@@ -25,16 +25,17 @@ def main():
             data = f.read()
 
         data = data.split("\n")
-        new_data = [f"<h1>{data[1]}</h1>"]
+        title = data[1]
+        new_data = [f"<h1>{title}</h1>"]
         for d in data[1:]:
-            if d.strip():
+            if d.strip() and d.strip().lower() != title.strip().lower():
                 new_data.append(f"<p>{d}</p>")
 
         # Write back using the original encoding
-        with open(chapter, 'w', encoding=detected_encoding, errors="replace") as f:
+        with open(chapter, "w", encoding=detected_encoding, errors="replace") as f:
             print(f"writing to {chapter.stem} with encoding {detected_encoding}")
             f.write("".join(new_data))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
